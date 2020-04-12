2d9b36ca208965a83cce83789ba553c3b78c2cd5
My name <sriharshanagulakonda@gmail.com>
2020-04-12 23:24:33 +0530

    holidays

--- cal/models.py
+++ cal/models.py
@@ -11,3 +11,10 @@ class Event(models.Model):
     def get_html_url(self):
         url = reverse('cal:event_edit', args=(self.id,))
         return f'<a href="{url}"> {self.title} </a>'
+
+class Task(models.Model):
+    eid=models.IntegerField()
+    task_name=models.CharField(max_length=200)
+    date=models.DateField()
+    status=models.CharField(max_length=100)
+    duration=models.IntegerField()
--- cal/utils.py
+++ cal/utils.py
@@ -2,6 +2,9 @@ from datetime import datetime, timedelta
 from calendar import HTMLCalendar
 # from .models import Event
 
+from datetime import date
+import holidays
+
 
 
 class Calendar(HTMLCalendar):
@@ -13,13 +16,16 @@ class Calendar(HTMLCalendar):
 	# formats a day as a td
 	# filter events by day
 	def formatday(self, day,i):
+		hol = holidays.India(years=self.year)
 		d = ''
-
+		hol=hol.get(str(self.month)+'-'+str(day)+'-'+str(self.year),'') if day!=0 else ''
+		if hol and day!=0:
+			return f"<td class='date-hol'><span class='date'>{day}{hol}</span></td>"
 		if day != 0 and (i!=5 and i!=6):
-			return f"<td><span class='date'>{day}</span></td>"
+			return f"<td ><a href='?month={self.year}-{self.month}'><span class='date'>{day}</span></a></td>"
 		elif day!=0:
 			return f"<td class='date-weekend'><span >{day}</span></td>"
-		return "<td class='date-empty'></td>"
+		return f"<td class='date-empty'></td>"
 
 	# formats a week as a tr
 	def formatweek(self, theweek):
@@ -28,15 +34,14 @@ class Calendar(HTMLCalendar):
 		for d, weekday in theweek:
 			week += self.formatday(d,i)
 			i+=1
-		return f'<tr> {week} </tr>'
+		return f"<tr> {week} </tr>"
 
 	# formats a month as a table
 	# filter events by year and month
 	def formatmonth(self, withyear=True):
-
 		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
 		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
-		cal += f'{self.formatweekheader()}\n'
+		cal += f"{self.formatweekheader()}\n"
 		for week in self.monthdays2calendar(self.year, self.month):
 			cal += f'{self.formatweek(week)}\n'
 		return cal
